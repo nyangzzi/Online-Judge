b1403c3c279e8e51c57132b6ce61c7320c77a2dc
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2019-10-19 03:36:37 +0900

    A/B

---
+++ C++/1008.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+
+	double A = 0;
+	double B = 0;
+	
+	cin >> A >> B;
+
+	cout << setprecision(9) << fixed;
+	cout << A / B << "\n";
+
+}
