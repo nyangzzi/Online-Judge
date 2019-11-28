16cf83a877e4c5b1dacfa291f99550f08b535e54
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2019-11-28 23:10:28 +0900

    A+B -7

---
+++ C++/11021.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int a = 0, b = 0;
+		cin >> a >> b;
+		cout << "Case #"<< i+1 << ": "<< a + b << endl;
+	}
+}
