428ada52c1d236ccfe05d2d998b74864e8825885
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-07-22 23:24:12 +0900

    점수 집계

---
+++ C++/9076.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int test = 0;
+	cin >> test;
+
+	while (test--) {
+		int arr[5] = {};
+
+		for (int i = 0; i < 5; i++) {
+			cin >> arr[i];
+		}
+
+		sort(arr, arr + 5);
+		if (arr[3] - arr[1] >= 4) {
+			cout << "KIN\n";
+		}
+		else {
+			cout << arr[1] + arr[2] + arr[3] << endl;
+		}
+	}
+}
