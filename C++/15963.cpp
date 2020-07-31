bff0b36a888b6e4aea29ba27aea64ae0328a2bd8
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-07-31 17:07:10 +0900

    CASIO

---
+++ C++/15963.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	long long n = 0, m = 0;
+	cin >> n >> m;
+
+	if (n == m) cout << 1 << endl;
+	else cout << 0 << endl;
+
+}
