4bcfa5c46546da2f3125deaa876a07b3777ef377
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-08-28 00:40:58 +0900

    ROT13

---
+++ C++/11655.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string str;
+
+	getline(cin, str);
+
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+			if ((str[i]+13) > 'z') {
+				str[i] -= 13;
+			}
+			else {
+				str[i] += 13;
+			}
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z') {
+			if ((str[i]+13) > 'Z') {
+				str[i] -= 13;
+			}
+			else {
+				str[i] += 13;
+			}
+		}
+		cout << str[i];
+	}
+}
