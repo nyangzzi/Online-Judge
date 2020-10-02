a54e5ddcf20786266ce24a280745e4f0a1656c53
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-10-02 23:12:30 +0900

    배수와 약수

---
+++ C++/5086.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	while (1) {
+		int a = 0, b = 0;
+		cin >> a >> b;
+
+		if (a == 0 && b == 0) break;
+
+		if (b % a == 0) {
+			cout << "factor\n";
+		}
+		else if (a%b == 0) {
+			cout << "multiple\n";
+		}
+		else {
+			cout << "neither\n";
+		}
+	}
+
+}
