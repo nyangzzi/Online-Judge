e82c0d73664fc1ad9296d87434172d369d4c7445
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-10-12 00:09:59 +0900

    Create 문자열을 정수로 바꾸기.java

---
+++ programmers/Level1/JAVA/문자열을 정수로 바꾸기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        if(s.charAt(0) == '-'){
+            for(int i =1; i<s.length(); i++){
+                answer *= 10;
+                answer += s.charAt(i) - '0';
+            }
+            answer = 0-answer;
+        }
+        else{
+            for(int i =0; i<s.length(); i++){
+                answer *= 10;
+                answer += s.charAt(i) - '0';
+            }
+        }
+        
+        return answer;
+    }
+}
