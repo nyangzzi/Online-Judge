0683e9e0abd787e362e01c114f05bf40f3fea174
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-10-15 23:56:57 +0900

    Create 자연수 뒤집어 배열로 만들기.java

---
+++ programmers/Level1/JAVA/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long n) {
+        String str = "" + n;
+        int[] answer = new int[str.length()];
+        
+       for(int i = 0; i<str.length(); i++){
+           answer[i] = (int)(n % 10);
+           n /=10;
+       }
+        return answer;
+    }
+}
