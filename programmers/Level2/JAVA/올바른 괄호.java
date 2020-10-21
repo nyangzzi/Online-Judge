0dbb302e51cfecef0cee76d3a397e838c3eeb152
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-10-21 17:52:46 +0900

    Create 올바른 괄호.java

---
+++ programmers/Level2/JAVA/올바른 괄호.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+  
+        int n = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '(') n++;
+            else n--;
+        
+            if(n < 0) break;
+        }
+    
+        if(n == 0) answer = true;
+        else answer = false;
+        return answer;
+
+    }
+}
