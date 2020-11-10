ba1e0c24880e3e90fc8f890f43e439c184921374
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-11-11 00:36:01 +0900

    Create 숫자의 표현.cpp

---
+++ programmers/Level2/CPP/숫자의 표현.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 1; i <= n;i++){
+        int u = 0;
+        for(int j = i; j <= n; j++){
+            u += j;
+            if(u == n) {answer++; break;}
+            else if(u > n) break;
+        }
+    }
+    
+    return answer;
+}
