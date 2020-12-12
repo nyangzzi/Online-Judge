c3de2baf197bfa30f9e0ef94689073aa9609d6fe
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-12-12 22:25:58 +0900

    Create 행렬의 곱셈.cpp

---
+++ programmers/Level2/CPP/행렬의 곱셈.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    
+    for(int i = 0; i<arr1.size(); i++){
+        vector<int> a;
+        for(int j = 0; j<arr2[0].size(); j++){
+            int sum = 0;
+            for(int k = 0; k < arr1[0].size(); k++){
+                sum += arr1[i][k] * arr2[k][j];
+            }
+            a.push_back(sum);
+        }
+        answer.push_back(a);
+    }
+    
+    return answer;
+}
