8234c3b56bb9229a412763b7aacd95146b5664ff
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-12-22 23:53:48 +0900

    Create 1545 거꾸로 출력해 보아요.cpp

---
+++ SWExpertAcademy/CPP/1545 거꾸로 출력해 보아요.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+		int n = 0;
+        cin >>n;
+		for(int i = n; i >=0; i--) {cout << i <<" ";}
+        cout << endl;
+	return 0;
+}
