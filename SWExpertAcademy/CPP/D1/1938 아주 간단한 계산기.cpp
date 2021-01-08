c36fc629e0383aa0a5639580630b2518ba87ae6b
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2021-01-08 11:46:43 +0900

    Create 1938 아주 간단한 계산기.cpp

---
+++ SWExpertAcademy/CPP/D1/1938 아주 간단한 계산기.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+	
+    int a = 0, b= 0;
+    cin >> a>>b;
+    
+    cout << a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b<<endl;
+    
+	return 0;
+}
