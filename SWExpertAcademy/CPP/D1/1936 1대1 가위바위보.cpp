8a0c204ef2123c53c03b895325cb01d41041ee89
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2021-01-09 19:44:18 +0900

    Create 1936 1대1 가위바위보.cpp

---
+++ SWExpertAcademy/CPP/D1/1936 1대1 가위바위보.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+	int a= 0, b=0;
+    cin >> a>>b;
+    
+    if(a>b) cout << "A"<<endl;
+    else cout << "B"<<endl;
+    
+    return 0;
+}
