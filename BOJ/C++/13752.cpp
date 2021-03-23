bbbff0b6828fdab6684a0465325c59fa5be8a134
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2021-03-23 22:54:12 +0900

    히스토그램

---
+++ BOJ/C++/13752.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int k = 0;
+		cin >> k;
+
+		for (int j = 0; j < k; j++) {
+			cout << "=";
+		}
+		cout << endl;
+  }
+}
