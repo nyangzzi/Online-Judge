a408921c8685a626a43e7db0197c1e47cc4e0599
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2021-03-27 00:31:12 +0900

    24

--- BOJ/C++/1408.cpp
+++ BOJ/C++/1408.cpp
@@ -1,5 +1,3 @@
-//수정 필요
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -13,15 +11,36 @@ int main(){
 	int nowTime[3] = {};
 	int startTime[3] = {};
 
-	nowTime[0] = (now[0] - '0') * 10 + (now[1] = '0');
-	nowTime[1] = (now[3] - '0') * 10 + (now[4] = '0');
-	nowTime[2] = (now[6] - '0') * 10 + (now[7] = '0');
+	nowTime[0] = (now[0] - '0') * 10 + (now[1] - '0');
+	nowTime[1] = (now[3] - '0') * 10 + (now[4] - '0');
+	nowTime[2] = (now[6] - '0') * 10 + (now[7] - '0');
 
-	startTime[0] = (start[0] - '0') * 10 + (start[1] = '0');
-	startTime[1] = (start[3] - '0') * 10 + (start[4] = '0');
-	startTime[2] = (start[6] - '0') * 10 + (start[7] = '0');
+	startTime[0] = (start[0] - '0') * 10 + (start[1] - '0');
+	startTime[1] = (start[3] - '0') * 10 + (start[4] - '0');
+	startTime[2] = (start[6] - '0') * 10 + (start[7] - '0');
 
+	int remain[3] = {};
+	
+	for (int i = 2; i >= 0; i--) {
+		remain[i] += startTime[i] - nowTime[i];
 
+		if (i == 0 && remain[i] < 0) {
+			remain[i] += 24;
+		}
+		else if (remain[i] < 0) {
+			remain[i - 1]--;
+			remain[i] += 60;
+		}
+	}
 
-	system("pause");
+	for (int i = 0; i < 3; i++) {
+		if (remain[i] < 10) {
+			cout << "0";
+		}
+		cout << remain[i];
+
+		if (i == 2) cout << endl;
+		else cout << ":";
+	}
+
 }
